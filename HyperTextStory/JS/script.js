6cfe85524c0d4fe5e4f735633c06d8aee8061cc8
Sheethal <sheethalshobowale@gmail.com>
2019-08-14 17:06:28 -0400

    hypertext

---
+++ HyperTextStory/JS/script.js
@@ -0,0 +1,7 @@
+img onmouseover="bigImg(this)"
+
+function show_coords(event) {
+  document.getElementById("demo").innerHTML = "X= " + event.clientX + "<br>Y= " + event.clientY;
+}
+
+onmousedown="show_coords(event)"
